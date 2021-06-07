db7e9cd7b748c0fcdbf5e2417b5654d463ff6a46
PC <PC@Drone19>
2021-06-07 10:25:40 +0900

    practice2

---
+++ src/HelloWorld/practice2.java
@@ -0,0 +1,14 @@
+package HelloWorld;
+
+public class practice2 {
+
+	public static void main(String[] args) {
+		// 1~100 까지 합 연산
+		int sum = 0;
+		for(int i = 0; i <= 100; i++)
+			sum = sum + i;
+
+		System.out.println("합: " +sum); 
+	}
+
+}
